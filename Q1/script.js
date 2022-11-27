2b9580d26a22b59f511b56dd3acb73a33c254332
Atefeh <at.bakhshi1373@gmail.com>
2022-11-27 14:10:26 +0330

    feat: add classes

---
+++ Q1/script.js
@@ -0,0 +1,72 @@
+"use strict";
+class Shape {
+  constructor(shapeName) {
+    this._shapeName = shapeName;
+  }
+  area() {}
+  perimeter() {}
+}
+
+class Polygon extends Shape {
+  constructor(height, width) {
+    super();
+    this._height = height;
+    this._width = width;
+  }
+  area() {
+    return this._height * this._width;
+  }
+  perimeter() {
+    return (this._height + this._width) * 2;
+  }
+}
+
+class NonPolygon extends Shape {
+  constructor(radius) {
+    super();
+    this._radius = radius;
+  }
+  area() {
+    let piNumber = 3.14;
+    return piNumber * this._radius ** 2;
+  }
+  perimeter() {
+    let piNumber = 3.14;
+    return piNumber * 2 * this._radius;
+  }
+}
+
+class Rectangle extends Polygon {
+  constructor() {
+    super();
+  }
+}
+class Square extends Polygon {
+  constructor() {
+    super();
+  }
+}
+
+class Circle extends NonPolygon {
+  constructor() {
+    super();
+  }
+}
+
+class Cylindrical extends Circle {
+  constructor(height) {
+    super();
+    this._height = height;
+  }
+
+  area() {
+    let piNumber = 3.14;
+    return 2 * piNumber * this._radius * (this._radius + this._height);
+  }
+  perimeter() {
+    let piNumber = 3.14;
+    return 2 * piNumber * this._radius;
+  }
+}
+
+
